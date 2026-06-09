ba00d430a59b867d59450aef1a9c9087f894a795
proyectoFlask_01 <sergioperulinux@gmail.com>
2026-06-08 20:44:16 -0500

    Subir build web para Vercel

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4109364051" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2907138108" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
